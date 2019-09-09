f57835c1a20e8144991980fa231464e0004ab17d
Steffen Sachse <steffen.sachse@uni-rostock.de>
2019-09-09 08:30:40 +0200

    Removed imports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { format, utcToZonedTime } from 'date-fns-tz';
-import { enUS } from 'date-fns/locale';
-import * as Discord from 'discord.js';
-import { concat, EMPTY, from, interval, Observable, of, Subscription } from 'rxjs';
-import { catchError, delay, finalize, map, mapTo, retryWhen, startWith, switchMap } from 'rxjs/operators';
 import TimezoneBot, { IBotConfig } from './bot/bot.class';
 
 const storage: any = require('node-persist');
